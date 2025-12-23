297f1ed414bbe3cbc9de6f87abed7204f0053e53
Whisper370 <2832415724@qq.com>
2025-12-23 19:04:46 +0800

    add line count function

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 #include "parser.h"
-int main() { int res = add(2,3); printf("2+3=%d
-", res); return 0; }
+int main() {
+    int res = add(2,3);
+    printf("2+3=%d
+", res);
+    // 行统计功能
+    printf("当前文件行数：%d
+", __LINE__);
+    return 0;
+}
