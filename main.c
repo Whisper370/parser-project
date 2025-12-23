02bdb5327a6728ace13fea4cbbbee45a857b10d1
Whisper370 <2832415724@qq.com>
2025-12-23 18:47:14 +0800

    initial commit: 实现加法解析功能

---
+++ main.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "parser.h"
+int main() { int res = add(2,3); printf("2+3=%d
+", res); return 0; }
---
+++ parser.h
@@ -0,0 +1,4 @@
+#ifndef PARSER_H
+#define PARSER_H
+int add(int a, int b);
+#endif
